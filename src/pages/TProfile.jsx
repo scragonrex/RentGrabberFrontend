650e9c7fea1a387fecf4360d65a355e1f8ae250b
scragonrex <scragondragon@gmail.com>
2023-09-18 20:24:43 +0530

    add course by teacher working

--- src/pages/TProfile.jsx
+++ src/pages/TProfile.jsx
@@ -3,30 +3,44 @@ import '../styles/TProfile.css'
 import { AddCircle } from '@mui/icons-material';
 import { Box, Modal, useMediaQuery } from '@mui/material';
 import { SelectMod } from '../components/Utils';
+import { useSelector } from "react-redux";
+
 const TProfile = () => {
+  const user = useSelector((state)=>state.auth.user);
+  const url = useSelector((state)=>state.auth.url);
+  const token = useSelector((state)=>state.auth.token);
   const mobileView = useMediaQuery('(max-width:720px)')
   const [modalOpen, setModalOpen] = useState(false);
   const [categoryList, setCategoryList] = useState(['Science', 'Mathematics','Social Science', 'English', 'Computer Science', 'Hindi', 'Commerce']);
   const [classList, setClassList] = useState(['6','7','8','9','10','11','12'])
   const [categoryValue, setCategoryValue] = useState();
-  const [classValue, setClassValue] = useState();
+  const [classValue, setClassValue] = useState(1);
   const [courseName, setCourseName] = useState();
+
   const handleCourseSubmit = async()=>{
     console.log(courseName, classValue, categoryValue);
-    setModalOpen(false)
+    console.log(user, token);
+
+    const response = await fetch(`${url}/teacher/addCourse/${user._id}`,{
+      method:"POST",
+      body:JSON.stringify({courseName, classValue, categoryValue}),
+      headers:{Authorization:`Bearer ${token}`, "Content-type":"application/json"}
+    });
+
+    const data = await response.json();
+    
   }
   const handleCategoryChange = (e)=>{
    setCategoryValue(e);
   }
-  const handleModalClose = ()=>{
-    setModalOpen(false)
-  }
+
   const handleClassChange = (e)=>{
     setClassValue(e);
   }
   const handleCourseName = (e)=>{
     setCourseName(e.target.value)
   }
+
   return (
     <div className='tProfile'>
       <div className="sidebar">
@@ -43,11 +57,11 @@ const TProfile = () => {
           <div className="box">Student enrolled: 35</div>
           <div className="box">Average rating: 7/10</div>
         </div>
-        <div className="addBtn" onClick={()=>setModalOpen(true)}>
-        <AddCircle sx={{ color: "rgb(6, 207, 106)", fontSize: "4rem" }}/>
+        <div className="addBtn" >
+        <AddCircle onClick={()=>setModalOpen(true)} sx={{ color: "rgb(6, 207, 106)", fontSize: "4rem" }}/>
         <Modal
         open={modalOpen}
-        onClose={handleModalClose}>
+        onClose={()=>setModalOpen(false)}>
         <div className="modalContainer">
           <div className="font-white font-subHeading" >Create your course</div>
           <input type="text" placeholder='Enter course name' className="inputCont" value={courseName} onChange={handleCourseName}/>
@@ -72,7 +86,7 @@ const TProfile = () => {
           <button className="btn" style={{ marginTop: "1rem" }} onClick={handleCourseSubmit}>Create</button>
         </div>
       </Modal>
-
+    
         </div>
       </div>
     </div>
